e155233ca796f457ffa69fbfda0a3cb44db8f455
rajeshkaratapu24 <rajeshkaratapu24@gmail.com>
2026-03-15 17:19:31 +0530

    Create bible_model.dart

---
+++ lib/bible/bible_model.dart
@@ -0,0 +1,9 @@
+class BibleVerse {
+  final String bookName;
+  final String chapter;
+  final String verseNumber;
+  final String text;
+  final bool isNewTestament;
+
+  BibleVerse({required this.bookName, required this.chapter, required this.verseNumber, required this.text, required this.isNewTestament});
+}
