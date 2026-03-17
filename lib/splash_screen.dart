152aadb20a0deb40e6676b14083859bcd296673a
rajeshkaratapu24 <rajeshkaratapu24@gmail.com>
2026-03-17 12:10:26 +0530

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:google_fonts/google_fonts.dart'; // గూగుల్ ఫాంట్స్ ఇంపోర్ట్
+import 'package:google_fonts/google_fonts.dart'; 
 import 'home_page.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,27 +10,12 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
     
-    // యానిమేషన్ కంట్రోలర్ (2 సెకన్ల పాటు జరుగుతుంది)
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-    
-    // స్మూత్ గా రావడానికి కరువ్ యానిమేషన్
-    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeOutBack);
-
-    // యానిమేషన్ స్టార్ట్ చేయి
-    _controller.forward();
-
-    // 3 సెకన్ల తర్వాత ఆటోమేటిక్ గా హోమ్ పేజీకి వెళ్ళిపోతుంది
+    // 3 సెకన్ల తర్వాత ఆటోమేటిక్ గా హోమ్ పేజీకి వెళ్ళిపోతుంది (యానిమేషన్స్ ఏమీ లేవు)
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
@@ -40,46 +25,34 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // బ్యాక్‌గ్రౌండ్ బ్లాక్
+      backgroundColor: Colors.black, 
       body: Center(
-        child: FadeTransition(
-          opacity: _controller, // నెమ్మదిగా కనిపిస్తుంది
-          child: ScaleTransition(
-            scale: _animation, // చిన్న సైజు నుంచి పెద్దగా అవుతుంది
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "WORLD OF GOD",
-                  style: GoogleFonts.ubuntu( // ఉబుంటు ఫాంట్ అప్లై చేశాం
-                    fontSize: 38,
-                    fontWeight: FontWeight.w300, // ఇది స్లిమ్ (Light) లుక్ ఇస్తుంది
-                    color: Colors.white,
-                    letterSpacing: 4,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "eco system",
-                  style: GoogleFonts.ubuntu(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w300, // ఇది కూడా స్లిమ్ గా
-                    color: Colors.greenAccent.withOpacity(0.9),
-                    letterSpacing: 3,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "WORLD OF GOD",
+              style: GoogleFonts.ubuntu( 
+                fontSize: 38,
+                fontWeight: FontWeight.w300, 
+                color: Colors.white,
+                letterSpacing: 4,
+              ),
             ),
-          ),
+            const SizedBox(height: 10),
+            Text(
+              "eco system",
+              style: GoogleFonts.ubuntu(
+                fontSize: 18,
+                fontWeight: FontWeight.w300, 
+                color: Colors.white, // గ్రీన్ తీసేశాం, నార్మల్ వైట్ పెట్టాం
+                letterSpacing: 3,
+                // ఇటాలిక్ తీసేశాం
+              ),
+            ),
+          ],
         ),
       ),
     );
