6d79335ab607e4f8c64be617c45011e5a32da329
rajeshkaratapu24 <rajeshkaratapu24@gmail.com>
2026-03-17 10:47:43 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") // 🚀 <-- ఫైర్‌బేస్ కోసం నేను కొత్తగా యాడ్ చేసిన లైన్ ఇదే!
 }
 
 android {
