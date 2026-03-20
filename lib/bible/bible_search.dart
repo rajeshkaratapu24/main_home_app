2ac1e593e2a54a6f2f75132d7cbe27a85687b867
rajeshkaratapu24 <rajeshkaratapu24@gmail.com>
2026-03-20 15:32:00 +0530

    Update bible_search.dart

--- lib/bible/bible_search.dart
+++ lib/bible/bible_search.dart
@@ -6,7 +6,6 @@ import 'bible_utils.dart';
 class BibleSearch extends StatefulWidget {
   final XmlDocument document;
   final String currentBook;
-
   const BibleSearch({super.key, required this.document, required this.currentBook});
 
   @override
@@ -20,24 +19,20 @@ class _BibleSearchState extends State<BibleSearch> {
   void _search(String query) {
     if (query.isEmpty || query.length < 2) return;
     List<Map<String, dynamic>> results = [];
-
     final books = widget.document.findAllElements('BIBLEBOOK');
     for (var book in books) {
       String bName = book.getAttribute('bname')!;
-      final chapters = book.findAllElements('CHAPTER');
-      for (var chapter in chapters) {
+      for (var chapter in book.findAllElements('CHAPTER')) {
         String cNum = chapter.getAttribute('cnumber')!;
-        final verses = chapter.findAllElements('VERS');
         int vIdx = 0;
-        for (var verse in verses) {
-          String vText = verse.innerText;
-          if (vText.contains(query)) {
+        for (var verse in chapter.findAllElements('VERS')) {
+          if (verse.innerText.contains(query)) {
             results.add({
               'book': bName,
               'chapter': cNum,
               'vNum': verse.getAttribute('vnumber'),
-              'text': vText.trim(),
-              'vIndex': vIdx, // Reading page లో ఆ వచనానికి వెళ్లడానికి
+              'text': verse.innerText.trim(),
+              'vIndex': vIdx, // వచనానికి వెళ్లడానికి ఇండెక్స్
             });
           }
           vIdx++;
@@ -56,22 +51,17 @@ class _BibleSearchState extends State<BibleSearch> {
         title: TextField(
           controller: _controller,
           style: const TextStyle(color: Colors.white),
-          decoration: const InputDecoration(hintText: "Search Bible...", hintStyle: TextStyle(color: Colors.white30), border: InputBorder.none),
+          decoration: const InputDecoration(hintText: "Search here...", border: InputBorder.none),
           onChanged: _search,
         ),
       ),
       body: ListView.builder(
         itemCount: searchResults.length,
-        itemBuilder: (context, i) {
-          final res = searchResults[i];
-          return ListTile(
-            title: Text("${BibleUtils.teluguBooks[res['book']]} ${res['chapter']}:${res['vNum']}", 
-                style: const TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.bold)),
-            subtitle: Text(res['text'], style: GoogleFonts.balooTammudu2(color: Colors.white70)),
-            // క్లిక్ చేయగానే డేటాతో సహా వెనక్కి వెళ్తుంది
-            onTap: () => Navigator.pop(context, res), 
-          );
-        },
+        itemBuilder: (context, i) => ListTile(
+          title: Text("${BibleUtils.teluguBooks[searchResults[i]['book']]} ${searchResults[i]['chapter']}:${searchResults[i]['vNum']}", style: const TextStyle(color: Colors.blueAccent)),
+          subtitle: Text(searchResults[i]['text'], style: GoogleFonts.balooTammudu2(color: Colors.white70)),
+          onTap: () => Navigator.pop(context, searchResults[i]), // డేటాని బ్యాక్ పంపిస్తుంది
+        ),
       ),
     );
   }
